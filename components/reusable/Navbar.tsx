94514b58a19d25e4bc7c7e9fb144967dfec5746c
Md Anamul Hasan <anamul15-3625@diu.edu.bd>
2025-04-24 10:28:30 +0600

    logo add

--- components/reusable/Navbar.tsx
+++ components/reusable/Navbar.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
@@ -5,7 +6,14 @@ export default function Navbar() {
   return (
     <div className="max-w-[1440px] px-4 md:px-10 xl:px-20 mx-auto">
       <Link href="/" className="flex items-center justify-start mt-5">
-        <img src="/logo.png" alt="Logo" />
+        <div className="flex flex-col">
+          <div className="w-[110px] mx-auto">
+            <Image className="w-full h-full" src="/logoImg.png" alt="Logo" width={300} height={200} />
+          </div>
+          <div className="w-[140px] mx-auto">
+            <Image className="w-full h-full" src="/logo.png" alt="Logo" width={200} height={200} />
+          </div>
+        </div>
       </Link>
     </div>
   );
